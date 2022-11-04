0286ab314fd3d2d00a6305380b2d351cd591cf26
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-04 06:33:56 +0000

    Adding tarfile member sanitization to extractall()

--- wordnet/languages/english.py
+++ wordnet/languages/english.py
@@ -160,7 +160,26 @@ def setup():
     if not os.path.exists(DB_PATH):
         urllib.request.urlretrieve(URL, GZ_PATH)
         with tarfile.open(GZ_PATH, 'r') as gz_f:
-            gz_f.extractall(DB_PATH)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(gz_f, DB_PATH)
     collector = SynsetCollector(DB_PATH)
     wordnet = WordNet(collector)
     return wordnet
